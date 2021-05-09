11f54a45487ca17289c20b88160ed9367c5b9332
lucas64500 <lucas64500@gmail.com>
2021-05-09 13:20:42 -0600

    Restore original code

--- src/app.py
+++ src/app.py
@@ -44,11 +44,9 @@ def add_page():
         client = NotionClient(token_v2)
         page = client.get_block(notebook_link)
 
-        return note_text
-
-        #today = date.today()
-        #new_page = page.children.add_new(PageBlock, title=today.strftime("%d/%m/%y") + ": " + note_title)
-        #new_page.children.add_new(TextBlock, title=note_text)
+        today = date.today()
+        new_page = page.children.add_new(PageBlock, title=today.strftime("%d/%m/%y") + ": " + note_title)
+        new_page.children.add_new(TextBlock, title=note_text)
 
         return 'The page added', 200
     except Exception:
