247a7eef1a71a12f6ae6b73b71f73c283e82e7d0
lucas64500 <lucas64500@gmail.com>
2021-05-14 20:42:39 -0600

    Add cron script

--- src/cron.py
+++ src/cron.py
@@ -1,4 +1,21 @@
+import requests
+
+
+def get_db_content(token, id):
+    headers = {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer '""+token+""'',
+        'Notion-Version': '2021-05-13'
+    }
+
+    data = ''#filters
+
+    response = requests.post('https://api.notion.com/v1/databases/'+id+'/query', headers=headers, data=data)
+
+    return response
+
 def cron(token):
 
-
-    return
+    db = get_db_content(token, '4b7625673a034a789bf055aabd180a0c')
+    print(db)
+    return db
